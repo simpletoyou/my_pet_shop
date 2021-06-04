0b5e6b5a0efa70a8c8f415119fe0c41dd788c14e
chenchuhua <3361694095@qq.com>
2021-06-04 17:39:00 +0800

    run sccessfully

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,18 +1,18 @@
 pragma solidity ^0.5.16;
 
 contract Adoption {
-  address[16] public adopters;
+  address[16] public adopters;//存领养者的地址
 
-  // Adopting a pet
+  // Adopting a pet 领养宠物
   function adopt(uint petId) public returns (uint) {
-    require(petId >= 0 && petId <= 15);
+    require(petId >= 0 && petId <= 15);  // 确保id在数组长度内
 
-    adopters[petId] = msg.sender;
+    adopters[petId] = msg.sender;// 保存调用这地址 
 
     return petId;
   }
 
-  // Retrieving the adopters
+  // Retrieving the adopters  返回领养者
   function getAdopters() public view returns (address[16] memory) {
     return adopters;
   }
--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,13 @@ App = {
   web3Provider: null,
   contracts: {},
 
-  init: async function() {
+  init: async function () {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
+    $.getJSON('../pets.json', function (data) {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
 
-      for (i = 0; i < data.length; i ++) {
+      for (i = 0; i < data.length; i++) {
         petTemplate.find('.panel-title').text(data[i].name);
         petTemplate.find('img').attr('src', data[i].picture);
         petTemplate.find('.pet-breed').text(data[i].breed);
@@ -23,32 +23,7 @@ App = {
     return await App.initWeb3();
   },
 
-  // initWeb3: async function() {
-  //   // Modern dapp browsers...
-  //   if (window.ethereum) {
-  //     App.web3Provider = window.ethereum;
-  //     try {
-  //       // Request account access
-  //       await window.ethereum.enable();
-  //     } catch (error) {
-  //       // User denied account access...
-  //       console.error("User denied account access")
-  //     }
-  //   }
-  //   // Legacy dapp browsers...
-  //   else if (window.web3) {
-  //     App.web3Provider = window.web3.currentProvider;
-  //   }
-  //   // If no injected web3 instance is detected, fall back to Ganache
-  //   else {
-  //     App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-  //   }
-  //   web3 = new Web3(App.web3Provider);
-
-  //   return App.initContract();
-  // },
-
-  initWeb3: function() {
+  initWeb3: function () {
     // Is there an injected web3 instance?
     if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider;
@@ -57,147 +32,92 @@ App = {
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
     web3 = new Web3(App.web3Provider);
-  
     return App.initContract();
   },
-  initContract: function() {
+  initContract: function () {
     // 加载Adoption.json，保存了Adoption的ABI（接口说明）信息及部署后的网络(地址)信息，它在编译合约的时候生成ABI，在部署的时候追加网络信息
-    $.getJSON('Adoption.json', function(data) {
+    $.getJSON('Adoption.json', function (data) {
+      console.log(data)
       // 用Adoption.json数据创建一个可交互的TruffleContract合约实例。
       var AdoptionArtifact = data;
       App.contracts.Adoption = TruffleContract(AdoptionArtifact);
-  
+
       // Set the provider for our contract
       App.contracts.Adoption.setProvider(App.web3Provider);
-  
+
       // Use our contract to retrieve and mark the adopted pets
       return App.markAdopted();
     });
     return App.bindEvents();
   },
 
-  // initContract: function() {
-  //   $.getJSON('Adoption.json', function(data) {
-  //     // Get the necessary contract artifact file and instantiate it with truffle-contract
-  //     var AdoptionArtifact = data;
-  //     App.contracts.Adoption = TruffleContract(AdoptionArtifact);
-
-  //     // Set the provider for our contract
-  //     App.contracts.Adoption.setProvider(App.web3Provider);
-
-  //     // Use our contract to retrieve and mark the adopted pets
-  //     return App.markAdopted();
-  //   });
-
-  //   return App.bindEvents();
-  // },
-
-  bindEvents: function() {
+  bindEvents: function () {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
 
-  // markAdopted: function(adopters, account) {
-  //   var adoptionInstance;
-
-  //   App.contracts.Adoption.deployed().then(function(instance) {
-  //     adoptionInstance = instance;
-
-  //     return adoptionInstance.getAdopters.call();
-  //   }).then(function(adopters) {
-  //     for (i = 0; i < adopters.length; i++) {
-  //       if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-  //         $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
-  //       }
-  //     }
-  //   }).catch(function(err) {
-  //     console.log(err.message);
-  //   });
-  // },
-  markAdopted: function(adopters, account) {
+  markAdopted: function (adopters, account) {
     var adoptionInstance;
-  
-    App.contracts.Adoption.deployed().then(function(instance) {
+
+    App.contracts.Adoption.deployed().then(function (instance) {
       adoptionInstance = instance;
-  
       // 调用合约的getAdopters(), 用call读取信息不用消耗gas
       return adoptionInstance.getAdopters.call();
-    }).then(function(adopters) {
+    }).then(function (adopters) {
       for (i = 0; i < adopters.length; i++) {
         if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
           $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
         }
       }
-    }).catch(function(err) {
+    }).catch(function (err) {
       console.log(err.message);
     });
   },
 
-  handleAdopt: function(event) {
+  handleAdopt: function (event) {
     event.preventDefault();
-  
+
     var petId = parseInt($(event.target).data('id'));
-  
+
     var adoptionInstance;
 
-    console.log('pid',petId)
-
-    console.log('web3.eth.getAccounts()',web3.eth.getAccounts())
     // 获取用户账号
-    web3.eth.getAccounts(function(error, accounts) {
-      if (error) {
-        console.log(error);
+    var web3Provider;
+    if (window.ethereum) {
+      web3Provider = window.ethereum;
+      try {
+        // 请求用户授权
+        window.ethereum.enable();
+      } catch (error) {
+        // 用户不授权时
+        console.error("User denied account access")
       }
+    } else if (window.web3) {   // 老版 MetaMask Legacy dapp browsers...
+      web3Provider = window.web3.currentProvider;
+    } else {
+      web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3js = new Web3(web3Provider);//web3js就是你需要的web3实例
 
-      console.log('accounts',accounts)
-    
+    web3js.eth.getAccounts(function (error, accounts) {
+      if (!error)
+      console.log(accounts[0])//授权成功后result能正常获取到账号了
       var account = accounts[0];
 
-      console.log('account',account)
-    
-      App.contracts.Adoption.deployed().then(function(instance) {
+      App.contracts.Adoption.deployed().then(function (instance) {
         adoptionInstance = instance;
-    
         // 发送交易领养宠物
-        return adoptionInstance.adopt(petId, {from: account});
-      }).then(function(result) {
+        return adoptionInstance.adopt(petId, { from: account });
+      }).then(function (result) {
         return App.markAdopted();
-      }).catch(function(err) {
+      }).catch(function (err) {
         console.log(err.message);
       });
     });
   }
-
-  // handleAdopt: function(event) {
-  //   event.preventDefault();
-
-  //   var petId = parseInt($(event.target).data('id'));
-
-  //   var adoptionInstance;
-
-  //   web3.eth.getAccounts(function(error, accounts) {
-  //     if (error) {
-  //       console.log(error);
-  //     }
-
-  //     var account = accounts[0];
-
-  //     App.contracts.Adoption.deployed().then(function(instance) {
-  //       adoptionInstance = instance;
-
-  //       // Execute adopt as a transaction by sending account
-  //       return adoptionInstance.adopt(petId, {from: account});
-  //     }).then(function(result) {
-  //       return App.markAdopted();
-  //     }).catch(function(err) {
-  //       console.log(err.message);
-  //     });
-  //   });
-  // }
-
 };
 
-$(function() {
-  $(window).load(function() {
+$(function () {
+  $(window).load(function () {
     App.init();
   });
 });
